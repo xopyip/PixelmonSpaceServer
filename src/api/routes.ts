10b3da69340ce05afcd74aef6783d42c9451b3cb
XopyIP <xopyip@gmail.com>
2020-01-27 02:43:19 +0100

    items list and find endpoints

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from "express";
 import PokemonController from "../controllers/pokemon.controller";
+import ItemsController from "../controllers/item.controller";
+
 const express = require('express');
 const router = express.Router();
 
@@ -9,6 +11,8 @@ router.get('/pokemon', PokemonController.list);
 router.get('/pokemon/sprite/:id', PokemonController.getSprite);
 router.get('/pokemon/:id(\\d+)', PokemonController.getByID);
 router.get('/pokemon/:name', PokemonController.getByName);
+router.get('/item', ItemsController.list);
+router.get('/item/:name', ItemsController.find);
 
 
 export default router;
---
+++ src/controllers/item.controller.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from "express";
+import {DropItemModel} from "../models/drops/item";
+
+class ItemsController {
+    public static async list(req: Request, res: Response) {
+        let find = DropItemModel.find({});
+        find.select("-_id item translations");
+        res.json(await find.exec());
+    }
+
+    public static async find(req: Request, res: Response) {
+        let find = DropItemModel.find({translations: {$in: [new RegExp(`.*${req.params.name}.*`, 'i')]}});
+        res.json((await find.exec()) || {error: "Not found"});
+    }
+}
+
+export default ItemsController;
